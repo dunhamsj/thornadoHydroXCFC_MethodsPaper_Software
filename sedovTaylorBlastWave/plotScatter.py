b27eff15ce7f616548eddede061cbf824a3736e4
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2025-10-17 16:57:10 -0400

    update plotScatter

--- sedovTaylorBlastWave/plotScatter.py
+++ sedovTaylorBlastWave/plotScatter.py
@@ -24,19 +24,16 @@ nDetCells = '03'
 field   = [ 'PF_D', \
             'PF_V', \
             'AF_P' ]
+mklabel = [ '3D', '2D' ]
 ylabel  = [ r'$\rho / \rho_{0}$', \
             r'$v    / v_{0}$', \
             r'$p    / p_{0}$' ]
-ylabelExact = [ r'$\rho_{\mathrm{Exact}} / \rho_{0}$', \
-                r'$v_{\mathrm{Exact}}    / v_{0}$', \
-                r'$p_{\mathrm{Exact}}    / p_{0}$' ]
+ylabelExact = r'$\mathrm{Exact}$'
 norm    = [ 5.0e3, \
             1.0e-3, \
             1.0e-3 ]
 
 xlim = np.array( [ -0.1, 1.1 ] )
-
-figName = '/home/dunhamsj/fig.stbw_scatter_nDetCells{:}.pdf'.format( nDetCells )
 
 # Specify directory containing amrex plotfiles
 plotfileDirectoryRoot = gv.dataDirectory
@@ -46,53 +43,57 @@ plotfileNameRoot = problemName + '.plt'
 
 plotfileDirectory \
   = [ plotfileDirectoryRoot \
-        + 'sedovTaylorBlastWave/{0:}_3D_nDetCells{1:}/' \
-          .format( problemName, nDetCells ), \
-      plotfileDirectoryRoot \
-        + 'sedovTaylorBlastWave/{0:}_2D_nDetCells{1:}/' \
-          .format( problemName, nDetCells ) ]
+        + 'sedovTaylorBlastWave/{1:}_{0:}_nDetCells{2:}/' \
+          .format( i, problemName, nDetCells ) for i in mklabel ]
 
 ### End of user input ###
 
-fig, ax = plt.subplots()
+exact = np.loadtxt( 'spherical_standard_omega0p00_nDetCells03_t500.dat', \
+                   skiprows = 2 )
+xEx   = exact[:,1]
+den   = exact[:,2]
+press = exact[:,4]
+vel   = exact[:,5]
+exact = [ den, vel, press ]
+color = gv.color#[ 'gold', 'blue', 'magenta' ] # complementary to gv.color
 
-marker = [ '.', '.' ]
+for j in range( len( field ) ) :
 
-for i in range( len( plotfileDirectory ) ) :
+    fig, ax = plt.subplots()
 
-    plotfileNumberArray \
-      = getPlotfileNumberArray \
-          ( plotfileDirectory[i],\
-            plotfileNameRoot )
-    
-    plotfileName \
-      = plotfileDirectory[i] \
-          + '{:}{:}'.format( plotfileNameRoot, \
-                             str( plotfileNumberArray[iSS] ).zfill( 8 ) )
-    
-    ds = yt.load( plotfileName )
-    
-    time = ds.current_time.to_ndarray()
+    for i in range( len( plotfileDirectory ) ) :
 
-    # Get lower and higher boundaries and convert them to numpy arrays
-    xL = ds.domain_left_edge.to_ndarray()
-    xH = ds.domain_right_edge.to_ndarray()
+        plotfileNumberArray \
+          = getPlotfileNumberArray \
+              ( plotfileDirectory[i],\
+                plotfileNameRoot )
+        
+        plotfileName \
+          = plotfileDirectory[i] \
+              + '{:}{:}'.format( plotfileNameRoot, \
+                                 str( plotfileNumberArray[iSS] ).zfill( 8 ) )
+        
+        ds = yt.load( plotfileName )
+        
+        time = ds.current_time.to_ndarray()
 
-    ad = ds.all_data()
-    x  = ad['X1_C'].to_ndarray()
-    y  = ad['X2_C'].to_ndarray()
-    if ( '2D' in plotfileDirectory[i] ) :
-        r = np.sqrt( x**2 + y**2 )
-    else:
-        z = ad['X3_C'].to_ndarray()
-        r = np.sqrt( x**2 + y**2 + z**2 )
+        # Get lower and higher boundaries and convert them to numpy arrays
+        xL = ds.domain_left_edge.to_ndarray()
+        xH = ds.domain_right_edge.to_ndarray()
 
-    # Restrict domain to 0 <= r <= 1
-#    ind = np.random.randint( 0, r.shape[0], 1000 )
-    ind = np.where( r <= 1.0 )[0]
-    r = r[ind]
+        ad = ds.all_data()
+        x = ad['X1_C'].to_ndarray()
+        y = ad['X2_C'].to_ndarray()
+        if ( '2D' in plotfileDirectory[i] ) :
+            r = np.sqrt( x**2 + y**2 )
+        else:
+            z = ad['X3_C'].to_ndarray()
+            r = np.sqrt( x**2 + y**2 + z**2 )
 
-    for j in range( len( field ) ) :
+        # Restrict domain to 0 <= r <= 1
+        ind = np.random.randint( 0, r.shape[0], 1000 )
+        ind = np.where( r <= 1.0 )[0]
+        r = r[ind]
 
         if ( field[j] == 'PF_V' ) :
             V1 = ad['PF_V1'].to_ndarray()[ind]
@@ -105,45 +106,36 @@ for i in range( len( plotfileDirectory ) ) :
         else:
             d = ad[field[j]].to_ndarray()[ind]
 
-        if ( i == 0 ) :
-            ax.scatter( r, d / norm[j], marker = marker[i], \
-                        edgecolors = gv.color[j], \
-                        facecolors = 'none', \
-                        label = ylabel[j] + '(3D)', \
-                        rasterized = True )
-        else:
-            ax.scatter( r, d / norm[j], marker = marker[i], \
-                        edgecolors = 'k', \
-                        facecolors = gv.color[j], \
-                        label = ylabel[j] + '(2D)', \
-                        rasterized = True )
+        ax.scatter( r, d / norm[j], \
+                    color = gv.color[i], \
+                    marker = '.', \
+                    label = mklabel[i], \
+                    rasterized = True )
 
-exact = np.loadtxt( 'spherical_standard_omega0p00_nDetCells03_t500.dat', \
-                   skiprows = 2 )
-x     = exact[:,1]
-den   = exact[:,2]
-press = exact[:,4]
-vel   = exact[:,5]
-color = gv.color#[ 'gold', 'blue', 'magenta' ] # complementary to gv.color
-ax.plot( x, den   / norm[0], '-', c = color[0], label = ylabelExact[0] )
-ax.plot( x, vel   / norm[1], '-', c = color[1], label = ylabelExact[1]  )
-ax.plot( x, press / norm[2], '-', c = color[2], label = ylabelExact[2]  )
+    y = [ ex / norm[j] for ex in exact[j] ]
+    ax.plot( xEx, y, 'k-', label = ylabelExact )
 
-ax.grid()
-ax.legend( loc = 2 )
+    ax.grid()
+    ax.legend( loc = 2 )
 
-ylim = ax.get_ylim()
-ax.text \
-  ( 0.45, 0.875 * ( ylim[1] - ylim[0] ), \
-    r'$t = {:}$'.format( np.int64( time ) ), \
-    bbox = dict( facecolor = 'white', edgecolor = 'black', \
-                 boxstyle = 'round' ) )
+    ylim = ax.get_ylim()
+    ax.text \
+      ( 0.45, 0.875 * ( ylim[1] - ylim[0] ), \
+        r'$t = {:}$'.format( np.int64( time ) ), \
+        bbox = dict( facecolor = 'white', edgecolor = 'black', \
+                     boxstyle = 'round' ) )
+    
+    ax.set_xlabel( r'$r$' )
+    ax.set_xlim( xlim )
 
-ax.set_xlabel( r'$r$' )
-ax.set_xlim( xlim )
+    ax.set_ylabel( ylabel[j] )
 
-if ( saveFig ) :
-    plt.savefig( figName, dpi = 300 )
-    print( '\n   Saved {:}'.format( figName ) )
-else:
-    plt.show()
+    if ( saveFig ) :
+        figName = '/home/dunhamsj/fig.stbw_scatter_nDetCells{:}_{:}.pdf' \
+                  .format( nDetCells, field[j] )
+        plt.savefig( figName, dpi = 300 )
+        print( '\n   Saved {:}'.format( figName ) )
+    else:
+        plt.show()
+
+    plt.close()
