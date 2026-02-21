3faf9f9c14f98c7de67b48068129e45a28a598e7
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2026-02-21 00:15:09 -0500

    tweaks to sinewaveadvection

--- sineWaveAdvection/computeL1Error.py
+++ sineWaveAdvection/computeL1Error.py
@@ -9,184 +9,94 @@ import globalVariables as gv
 
 from gaussxw import gaussxw
 
-nQ = 10
+pathToData = gv.dataDirectory + 'SineWaveAdvection/'
+fileNameRoot = pathToData + 'Advection1D_SineWaveX1'
 
-def Lagrange( x, xq, i ):
+def Lagrange(x, xq, i):
     L = 1.0
-    for j in range( xq.shape[0] ):
+    for j in range(xq.shape[0]):
         if i != j:
-            L *= ( x - xq[j] ) / ( xq[i] - xq[j] )
+            L *= (x - xq[j]) / (xq[i] - xq[j])
     return L
 
-def rho_h( x, xq, rhoq ):
-    rho = 0.0
-    for i in range( xq.shape[0] ):
-        rho += rhoq[i] * Lagrange( x, xq, i )
-    return rho
+def uh(x, xq, uq):
+    u = 0.0
+    for i in range(xq.shape[0]):
+        u += uq[i] * Lagrange(x, xq, i)
+    return u
 
-def rhoExact( x ):
-    return 1.0 + 0.1 * np.sin( 2.0 * np.pi * x )
+def D_exact(x):
+    W = 1.0 / np.sqrt(1.0 - 0.1**2) 
+    return W * (1.0 + 0.1 * np.sin(2.0 * np.pi * x))
 
-def ComputeL1( pathToData, fileNameRoot, nN, nXC, grid, fc ):
+def get_data(nN, nXC, grid, FC, suffix):
 
-    N = np.int64( nN )
+    fileName = fileNameRoot \
+                 + '_nN{0:}_nXC{1:}_{2:}{3:}/' \
+                   .format(str(nN).zfill(2), str(nXC).zfill(4), grid, FC, suffix) \
+                 + 'Advection1D_SineWaveX1_nN{0:}_nXC{1:}_{2:}{3:}.{4:}/CF_D_proc000.dat' \
+                   .format(str(nN).zfill(2), str(nXC).zfill(4), grid, FC, suffix)
+    print(fileName)
 
-    fileRoot \
-      = '{:}{:}_nN{:}_nXC{:}_{:}{:}.plt' \
-            .format( pathToData, fileNameRoot, nN, nXC, grid, fc )
 
-    fileI = fileRoot + '_init.dat'
+    nN = np.array([nN, 1, 1], dtype = np.int64)
 
-    dataI = np.loadtxt( fileI )
-    i = 0
-    levels = np.array( dataI[:,i]    , dtype = np.int64   ); i += 1
-    iX1    = np.array( dataI[:,i]    , dtype = np.int64   ); i += 1
-    iX2    = np.array( dataI[:,i]    , dtype = np.int64   ); i += 1
-    iX3    = np.array( dataI[:,i]    , dtype = np.int64   ); i += 1
-    dX1    = np.array( dataI[:,i]    , dtype = np.float64 ); i += 1
-    dX2    = np.array( dataI[:,i]    , dtype = np.float64 ); i += 1
-    dX3    = np.array( dataI[:,i]    , dtype = np.float64 ); i += 1
-    X1n    = np.array( dataI[:,i:i+N], dtype = np.float64 ); i += N
-    X2n    = np.array( dataI[:,i:i+1], dtype = np.float64 ); i += 1
-    X3n    = np.array( dataI[:,i:i+1], dtype = np.float64 ); i += 1
-    dataI  = np.array( dataI[:,i:i+N], dtype = np.float64 )
+    data = np.loadtxt(fileName)
 
-    nX1 = iX1.shape[0]
-    nDOFX = nX1 * N
+    j = 0
+    iLevel = np.int64  ( data[:,j]         ); j += 1
+    iX1    = np.int64  ( data[:,j]         ); j += 1
+    iX2    = np.int64  ( data[:,j]         ); j += 1
+    iX3    = np.int64  ( data[:,j]         ); j += 1
+    dx1    = np.float64( data[:,j]         ); j += 1
+    dx2    = np.float64( data[:,j]         ); j += 1
+    dx3    = np.float64( data[:,j]         ); j += 1
+    x1n    = np.float64( data[:,j:j+nN[0]] ); j += nN[0]
+    x2n    = np.float64( data[:,j:j+nN[1]] ); j += nN[1]
+    x3n    = np.float64( data[:,j:j+nN[2]] ); j += nN[2]
+    un     = data[:,j:]
 
-    fileF = fileRoot + '_finl.dat'
+    return iLevel.shape[0], dx1, x1n, un
 
-    dataF = np.loadtxt( fileF )
-    i = 0
-    levels = np.array( dataF[:,i]    , dtype = np.int64   ); i += 1
-    iX1    = np.array( dataF[:,i]    , dtype = np.int64   ); i += 1
-    iX2    = np.array( dataF[:,i]    , dtype = np.int64   ); i += 1
-    iX3    = np.array( dataF[:,i]    , dtype = np.int64   ); i += 1
-    dX1    = np.array( dataF[:,i]    , dtype = np.float64 ); i += 1
-    dX2    = np.array( dataF[:,i]    , dtype = np.float64 ); i += 1
-    dX3    = np.array( dataF[:,i]    , dtype = np.float64 ); i += 1
-    X1n    = np.array( dataF[:,i:i+N], dtype = np.float64 ); i += N
-    X2n    = np.array( dataF[:,i:i+1], dtype = np.float64 ); i += 1
-    X3n    = np.array( dataF[:,i:i+1], dtype = np.float64 ); i += 1
-    dataF  = np.array( dataF[:,i:i+N], dtype = np.float64 )
+nQ = 10
+xqq, wq = gaussxw(nQ)
+mass0 = 0.0
+xl = 0.0
+dx = 1.0 / 1024
+for i in range(1024):
+    xC = xl + 0.5 * dx
+    xq = xC + dx * xqq
+    mass0 += dx * np.sum(wq * np.abs(D_exact(xq)))
+    xl += dx
 
-    xqN, wqN = gaussxw( N  )
-    xqQ, wqQ = gaussxw( nQ )
+def computeL1error(nN, nXC, grid, FC):
 
-    xL = 0.0
-    Lnum = 0.0
-    Lden = 0.0
-    for iX1 in range( nX1 ):
+    nX, dx, xn, un = get_data(nN, nXC, grid, FC, 'final')
 
-        xH = xL + dX1[iX1]
-        xC = 0.5 * ( xL + xH )
-        x = xC + xqQ * dX1[iX1]
-        xL = xH
+    nQ = nN
+    xqq, wq = gaussxw(nQ)
 
-        uqN_F = dataF[iX1]
-        for q in range( nQ ):
-            Lnum += dX1[iX1] * wqQ[q] * np.abs( rho_h( x[q], xqN, uqN_F ) - rhoExact( x[q] ) )
-            Lden += dX1[iX1] * wqQ[q] * np.abs( rhoExact( x[q] ) )
+    L1 = 0.0
+    xl = 0.0
+    for i in range(nX):
+        xC = xl + 0.5 * dx[i]
+        xq = xC + dx[i] * xqq
+        L1 += dx[i] * np.sum(wq * np.abs(uh(xq, xn[i], un[i]) - D_exact(xq)))
+        xl += dx[i]
 
-    L1 = Lnum / Lden
+    return nN, nX, L1 / mass0
 
-    return N, nX1, L1
+data = [[2, 32 , 'Single', ''    ], \
+        [2, 32 , 'Multi' , '_FCF'], \
+        [2, 32 , 'Multi' , '_FCT'], \
+        [2, 128, 'Single', ''   ], \
+        [3, 32 , 'Single', ''    ], \
+        [3, 32 , 'Multi' , '_FCF'], \
+        [3, 32 , 'Multi' , '_FCT'], \
+        [3, 128, 'Single', ''   ]]
 
-fig, ax = plt.subplots( 1, 1 )
-
-nn = [ '02', '03' ]
-
-pathToData = gv.dataDirectory + 'SineWaveAdvection/'
-fileNameRoot = 'Advection1D_SineWaveX1'
-
-filename = gv.paperDirectory + 'FluxCorrections_L1.tex'
-with open( filename, 'w' ) as f:
-    f.write( '% Filename: {:}\n'.format( filename.split( '/' )[-1] ) )
-    f.write( '% Generated by {:}\n% on {:}\n' \
-             .format( __file__, datetime.today() ) )
-    f.write( '\\begin{deluxetable}{ccccc}[htb!]\n' )
-    f.write( '  \\tablecaption{Effect of Flux Corrections}\n' )
-    f.write( '  \\tablehead{                %\n' )
-    f.write( '  \\colhead{$N$}              &\n' )
-    f.write( '  \\colhead{$\\NK$}            &\n' )
-    f.write( '  \\colhead{Mesh}             &\n' )
-    f.write( '  \\colhead{Flux Corrections} &\n' )
-    f.write( '  \\colhead{$E_{1}\\left[\\rho_{h}\\right]$} }\n' )
-    f.write( '  \\startdata\n' )
-
-for i in range( len( nn ) ):
-
-    narr  = []
-    nxarr = []
-    L1arr = []
-    garr  = []
-    fcarr = []
-
-    nN = nn[i]
-
-    nXC = '0032'
-
-    N, nX1, L1 \
-      = ComputeL1( pathToData, fileNameRoot, nN, nXC, 'Single', '' )
-    narr .append( N   )
-    nxarr.append( nX1 )
-    L1arr.append( L1  )
-    garr .append( 'Single' )
-    fcarr.append( 'N/A' )
-
-    N, nX1, L1 \
-      = ComputeL1( pathToData, fileNameRoot, nN, nXC, 'Multi', '_FCT' )
-    narr .append( N   )
-    nxarr.append( nX1 )
-    L1arr.append( L1  )
-    garr .append( 'Multi' )
-    fcarr.append( 'Yes' )
-
-    N, nX1, L1 \
-      = ComputeL1( pathToData, fileNameRoot, nN, nXC, 'Multi', '_FCF' )
-    narr .append( N   )
-    nxarr.append( nX1 )
-    L1arr.append( L1  )
-    garr .append( 'Multi' )
-    fcarr.append( 'No' )
-
-    nXC = '0064'
-
-    N, nX1, L1 \
-      = ComputeL1( pathToData, fileNameRoot, nN, nXC, 'Single', '' )
-    narr .append( N   )
-    nxarr.append( nX1 )
-    L1arr.append( L1  )
-    garr .append( 'Single' )
-    fcarr.append( 'N/A' )
-
-    with open( filename, 'a' ) as f:
-        for i in range( len( L1arr ) ):
-            exponent = np.floor( np.log10( np.abs( L1arr[i] ) ) )
-            f.write( '{:} & {:} & {:} & {:} & ${:.2f}\\times10^{{{:}}}$ \\\\\n' \
-                     .format( narr[i], nxarr[i], garr[i], fcarr[i], \
-                              L1arr[i] / 10**( exponent ), np.int64( exponent ) ) )
-
-    plt.loglog( np.array(narr)*np.array(nxarr), L1arr, '.', label = f'nN = {nN}' )
-
-plt.legend()
-plt.show()
-
-with open( filename, 'a' ) as f:
-    f.write( '  \\enddata\n' )
-    f.write( '  \\label{tab.CR}\n' )
-    f.write( '  \\tablecomments{Effects of flux corrections on the accuracy\n' )
-    f.write( 'of our numerical method.\n' )
-    f.write( 'The first column is the number of DG nodes per element,\n' )
-    f.write( 'the second column is the number of elements,\n' )
-    f.write( 'the third column specifies whether or not the simulation used a single-\n' )
-    f.write( 'or multi-level mesh,\n' )
-    f.write( 'the fourth column specifies whether or not a multi-level mesh simulation\n' )
-    f.write( 'applied flux corrections,\n' )
-    f.write( 'and the fifth column denotes the error as defined in\n' )
-    f.write( '\\myeqref{eq.Error}.}\n' )
-    f.write( '\\end{deluxetable}\n' )
-print( '\n  Saved {:}'.format( filename ) )
-
+for i in range(len(data)):
+    L1 = computeL1error(data[i][0], data[i][1], data[i][2], data[i][3])
+    print(data[i], L1[-1])
 import os
 os.system( 'rm -rf __pycache__ ' )
--- sineWaveAdvection/plotConservation.py
+++ sineWaveAdvection/plotConservation.py
@@ -8,71 +8,88 @@ plt.style.use( 'publication.sty' )
 from getNodalData import getData, getQuadratureWeights1D
 from myUtilitiesModule import getPlotfileNumberArray, Lagrange, rho_h
 
+from gaussxw import gaussxw
+
 import globalVariables as gv
 
-rootPathToData = gv.dataDirectory + 'sineWaveAdvection/'
-rootPathToData = '/mnt/shared/work/codes/thornado/SandBox/AMReX/dgExperiments_Euler_Relativistic_IDEAL/'
+rootPathToData = gv.dataDirectory + 'SineWaveAdvection/'
+
+def get_data(nN, fileName):
+
+    nN = np.array([nN, 1, 1], dtype = np.int64)
+
+    data = np.loadtxt(fileName+'CF_D_proc000.dat')
+
+    j = 0
+    iLevel = np.int64  ( data[:,j]         ); j += 1
+    iX1    = np.int64  ( data[:,j]         ); j += 1
+    iX2    = np.int64  ( data[:,j]         ); j += 1
+    iX3    = np.int64  ( data[:,j]         ); j += 1
+    dx1    = np.float64( data[:,j]         ); j += 1
+    dx2    = np.float64( data[:,j]         ); j += 1
+    dx3    = np.float64( data[:,j]         ); j += 1
+    x1n    = np.float64( data[:,j:j+nN[0]] ); j += nN[0]
+    x2n    = np.float64( data[:,j:j+nN[1]] ); j += nN[1]
+    x3n    = np.float64( data[:,j:j+nN[2]] ); j += nN[2]
+    un     = data[:,j:]
+
+    return iLevel.shape[0], dx1, x1n, un
 
 def exact(x):
     W = 1.0 / np.sqrt(1.0 - 0.1**2)
     return W * (1.0 + 0.1 * np.sin( 2.0 * np.pi * x ))
 
-data = [['02', '0128', 'Single', '', '2single'], \
-        ['02', '0032', 'Multi', '_FCF','2multi-fcf'], \
-        ['02', '0032', 'Multi', '_FCT','2multi-fct'], \
-        ['03', '0128', 'Single', '', '3single'], \
-        ['03', '0032', 'Multi', '_FCF','3multi-fcf'], \
-        ['03', '0032', 'Multi', '_FCT','3multi-fct']]
-data = [['01', '0032', 'Single', '', 'DG(0), nX=032'], \
-        ['01', '0256', 'Single', '', 'DG(0), nX=256'], \
-        ['02', '0032', 'Single', '', 'DG(1), nX=032'], \
-        ['02', '0256', 'Single', '', 'DG(1), nX=256'], \
-        ['03', '0032', 'Single', '', 'DG(2), nX=032'], \
-        ['03', '0256', 'Single', '', 'DG(2), nX=256']]
+data = [[2, 32 , 'Single', ''    , 'DG(1), nXC = 32'], \
+        [2, 32 , 'Multi' , '_FCF', 'DG(1), nXC = 32 (F)'], \
+        [2, 32 , 'Multi' , '_FCT', 'DG(1), nXC = 32 (T)'], \
+        [2, 128, 'Single', ''    , 'DG(1), nXC = 128'], \
+        [3, 32 , 'Single', ''    , 'DG(2), nXC = 32'], \
+        [3, 32 , 'Multi' , '_FCF', 'DG(2), nXC = 32 (F)'], \
+        [3, 32 , 'Multi' , '_FCT', 'DG(2), nXC = 32 (T)'], \
+        [3, 128, 'Single', ''    , 'DG(2), nXC = 128']]
 
-gv.dataDirectory + 'sineWaveAdvection/'
 
 def computeMass(un, dx, wq):
     mass = 0.0
-    iLo = 0
     for iX1 in range(dx.shape[0]):
-        iHi = iLo + wq.shape[0]
-        mass += dx[iX1] * np.sum(wq * np.abs(un[iLo:iHi]))
-        iLo = iHi
+        mass += dx[iX1] * np.sum(wq * np.abs(un[iX1]))
     return mass
+
+nQ = 10
+xqq, wq = gaussxw(nQ)
+mass0 = 0.0
+xl = 0.0
+dx = 1.0 / 1024
+for i in range(1024):
+    xC = xl + 0.5 * dx
+    xq = xC + dx * xqq
+    mass0 += dx * np.sum(wq * np.abs(exact(xq)))
+    xl += dx
 
 def plot(nN, nXC, grid, fc, lab):
 
     id = 'Advection1D_SineWaveX1_nN{:}_nXC{:}_{:}{:}' \
-         .format(nN, nXC, grid, fc)
+         .format(str(nN).zfill(2), str(nXC).zfill(4), grid, fc)
     dataDirectory = rootPathToData + id + '/'
-
-    print(dataDirectory)
 
     plotfileNumberArray = getPlotfileNumberArray( dataDirectory, id, swa = True )
 
     nSS = plotfileNumberArray.shape[0]
 
-    wq = getQuadratureWeights1D(int(nN))
+    wq = getQuadratureWeights1D(nN)
 
     dm = np.empty(nSS, np.float64)
 
-    d = dataDirectory + '{:}.plt{:}_nodal/' \
-                        .format(id, str(plotfileNumberArray[0]).zfill(8))
-    xn, un0, dx = getData(d, 'CF_D', [int(nN), 1, 1])
-    mass0 = computeMass(exact(xn), dx, wq)
     for iSS in range(nSS):
-        d = dataDirectory + '{:}.plt{:}_nodal/' \
+        filename = dataDirectory + '{:}.plt{:}_nodal/' \
                             .format(id, str(plotfileNumberArray[iSS]).zfill(8))
-        xn, un, dx = getData(d, 'CF_D', [int(nN), 1, 1])
+        nX, dx, xn, un = get_data(nN, filename)
 
         dm[iSS] = computeMass(un, dx, wq)
 
-    d = dataDirectory + '{:}.plt{:}_nodal/' \
-                        .format(id, str(plotfileNumberArray[-1]).zfill(8))
-    xn, un, dx = getData(d, 'CF_D', [int(nN), 1, 1])
-    t = np.linspace(0,10,dm.shape[0])
-    plt.plot(t,(dm-mass0)/mass0,label=lab)
+    t = np.linspace(0, 10, dm.shape[0])
+
+    plt.plot(t,np.abs(dm-mass0)/mass0,label=lab)
 
 for i in range(len(data)):
     plot(data[i][0],data[i][1],data[i][2],data[i][3],data[i][4])
@@ -83,9 +100,9 @@ plt.ylabel(r'$\left|M(t)-M(0)\right|/M(0)$')
 plt.yscale('log')
 #plt.ylim(-1.0e-14, 1.0e-14)
 plt.legend()
-#plt.show()
-figname = '/home/dunhamsj/fig.png'
-plt.savefig(figname, dpi = 300)
-print('  Saved {:}'.format(figname))
+plt.show()
+#figname = '/Users/dunhamsj/fig.png'
+#plt.savefig(figname, dpi = 300)
+#print('  Saved {:}'.format(figname))
 import os
 os.system('rm -rf __pycache__')
