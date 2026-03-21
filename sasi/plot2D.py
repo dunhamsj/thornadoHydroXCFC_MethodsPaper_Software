59d9789753f3af3f6756ab14e9c60ce3206577b0
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2026-03-21 01:20:20 -0400

    tweaks

--- sasi/plot2D.py
+++ sasi/plot2D.py
@@ -16,7 +16,6 @@ import globalVariables as gv
 # Specify name of problem
 problemName = 'StandingAccretionShock_XCFC'
 
-#plotfileDirectory = '/Users/dunhamsj/sasi_nX0_096_tagOnShocksAndRlLT21/'
 plotfileDirectory = gv.dataDirectory + 'sasi/'
 
 # Specify plot file base name
@@ -45,13 +44,13 @@ ShowMesh  = True
 MeshAlpha = 0.05
 
 # Outer boundary of plot
-xH = 1.25e2
+xH = 1.5e2
 
 # Colorbar limits
 vmin = 1.0e7
 vmax = 3.0e10
 
-SS = [ 0, -1 ]
+SS = [ 0, 1, 3, 2 ]
 
 ### End of user input ###
 
@@ -63,86 +62,178 @@ plotfileNumberArray \
       (plotfileDirectory, \
        plotfileNameRoot)
 
+fig, ax = plt.subplots()
+ax.grid(False)
+ax.set_xlim([-xH, xH])
+ax.set_ylim([-xH, xH])
+ax.set_xlabel(xLabel, fontsize = 16)
+ax.set_ylabel(yLabel, fontsize = 16)
+
 # Adapted from
 # https://stackoverflow.com/questions/28752727/
 # map-values-to-colors-in-matplotlib
 norm = mpl.colors.LogNorm(vmin = vmin, vmax = vmax, clip = True)
 mapper = mpl.cm.ScalarMappable(norm = norm, cmap = cmap)
 
-for iSS in SS:
+pe = 1
+figName = figNameRoot + '.pdf'
+    
+for j in range(len(SS)):
+
+    iSS = SS[j]
 
     ds = yt.load(plotfileDirectory + plotfileNameRoot \
                     + str(plotfileNumberArray[iSS]).zfill(8))
     
     time = np.round(ds.current_time.to_ndarray())
 
-    figName = figNameRoot + '_t{:}.pdf'.format(str(np.int64(time)).zfill(2))
-    
     ad = ds.all_data()
     r      = ad['X1_C'].to_ndarray()
     theta  = ad['X2_C'].to_ndarray()
     dr     = ad['dX1' ].to_ndarray()
     dtheta = ad['dX2' ].to_ndarray()
     d = ad[field].to_ndarray()
+    print(d.shape)
     
-    fig, ax = plt.subplots()
-    ax.grid(False)
-    ax.set_xlim([0.0, xH])
-    ax.set_ylim([-xH, xH])
-    ax.set_xlabel(xLabel, fontsize = 16)
-    ax.set_ylabel(yLabel, fontsize = 16)
+    if (j == 0):
+        ax.text(0.02, 0.94, r'$t = {:.0f}\ \mathrm{{ms}}$'.format(time), \
+                fontsize = 12, transform = ax.transAxes)
+    if (j == 1):
+        ax.text(0.78, 0.94, r'$t = {:.0f}\ \mathrm{{ms}}$'.format(time), \
+                fontsize = 12, transform = ax.transAxes)
+    if (j == 2):
+        ax.text(0.02, 0.04, r'$t = {:.0f}\ \mathrm{{ms}}$'.format(time), \
+                fontsize = 12, transform = ax.transAxes)
+    if (j == 3):
+        ax.text(0.78, 0.04, r'$t = {:.0f}\ \mathrm{{ms}}$'.format(time), \
+                fontsize = 12, transform = ax.transAxes)
+    
+    for i in range(0, d.shape[0], pe):
+        if(r[i] + 0.5 * dr[i] > xH): continue
 
-    ax.text(0.6, 0.92, r'$t = {:.0f}\ \mathrm{{ms}}$'.format(time), fontsize = 12, transform = ax.transAxes)
-    
-    for i in range(0, d.shape[0]):
-        if(r[i]>xH): continue
-        rgba = mapper.to_rgba(d[i])
-    
+        skip = True
+
+        x = r[i] * np.sin(theta[i])
+        z = r[i] * np.cos(theta[i])
+
+        if ((j == 0) | (j == 2)): x *= -1.0
+
+        if((j == 0) & ((z > 0.0) & (x < 0.0))): skip = False
+        if((j == 1) & ((z > 0.0) & (x > 0.0))): skip = False
+        if((j == 2) & ((z < 0.0) & (x < 0.0))): skip = False
+        if((j == 3) & ((z < 0.0) & (x > 0.0))): skip = False
+
+        if (skip): continue
+
+        rL = r[i] - 0.5 * dr[i]
+        rH = r[i] + 0.5 * dr[i]
+        tL = theta[i] - 0.5 * dtheta[i]
+        tH = theta[i] + 0.5 * dtheta[i]
+
         # Specify corners of polygon
-        x11 = r[i] * np.sin(theta[i] - 0.5 * dtheta[i])
-        x12 = (r[i] + dr[i]) * np.sin(theta[i] - 0.5 * dtheta[i])
-        x22 = r[i] * np.sin(theta[i] + 0.5 * dtheta[i])
-        x21 = (r[i] + dr[i]) * np.sin(theta[i] + 0.5 * dtheta[i])
-        z11 = r[i] * np.cos(theta[i] - 0.5 * dtheta[i])
-        z12 = (r[i] + dr[i]) * np.cos(theta[i] - 0.5 * dtheta[i])
-        z22 = r[i] * np.cos(theta[i] + 0.5 * dtheta[i])
-        z21 = (r[i] + dr[i]) * np.cos(theta[i] + 0.5 * dtheta[i])
+        if ((j == 1) | (j == 3)):
+            x11 = rL * np.sin(tL)
+            z11 = rL * np.cos(tL)
+            x12 = rH * np.sin(tL)
+            z12 = rH * np.cos(tL)
+            x21 = rH * np.sin(tH)
+            z21 = rH * np.cos(tH)
+            x22 = rL * np.sin(tH)
+            z22 = rL * np.cos(tH)
+        else:
+            x11 = -rL * np.sin(tL)
+            z11 = rL * np.cos(tL)
+            x12 = -rH * np.sin(tL)
+            z12 = rH * np.cos(tL)
+            x21 = -rH * np.sin(tH)
+            z21 = rH * np.cos(tH)
+            x22 = -rL * np.sin(tH)
+            z22 = rL * np.cos(tH)
     
         xy = list(zip([x11, x12, x21, x22], [z11, z12, z21, z22]))
     
+        rgba = mapper.to_rgba(d[i])
+
         ax.add_patch(mpl.patches.Polygon \
                        (xy = xy, fill = True, fc = rgba, ec = rgba))
+
+if (ShowMesh):
+    for j in range(len(SS)):
+
+        iSS = SS[j]
     
-    if (ShowMesh):
-        for i in range(0, d.shape[0]):
-            if(r[i]>xH): continue
+        ds = yt.load(plotfileDirectory + plotfileNameRoot \
+                        + str(plotfileNumberArray[iSS]).zfill(8))
         
+        ad = ds.all_data()
+        r      = ad['X1_C'].to_ndarray()
+        theta  = ad['X2_C'].to_ndarray()
+        dr     = ad['dX1' ].to_ndarray()
+        dtheta = ad['dX2' ].to_ndarray()
+        d = ad[field].to_ndarray()
+        
+        for i in range(0, d.shape[0], pe):
+            if(r[i] > xH): continue
+
+            skip = True
+
+            x = r[i] * np.sin(theta[i])
+            z = r[i] * np.cos(theta[i])
+
+            if ((j == 0) | (j == 2)): x *= -1.0
+
+            if((j == 0) & ((z > 0.0) & (x < 0.0))): skip = False
+            if((j == 1) & ((z > 0.0) & (x > 0.0))): skip = False
+            if((j == 2) & ((z < 0.0) & (x < 0.0))): skip = False
+            if((j == 3) & ((z < 0.0) & (x > 0.0))): skip = False
+
+            if (skip): continue
+
+            rL = r[i] - 0.5 * dr[i]
+            rH = r[i] + 0.5 * dr[i]
+            tL = theta[i] - 0.5 * dtheta[i]
+            tH = theta[i] + 0.5 * dtheta[i]
+
             # Specify corners of polygon
-            x11 = r[i] * np.sin(theta[i] - 0.5 * dtheta[i])
-            x12 = (r[i] + dr[i]) * np.sin(theta[i] - 0.5 * dtheta[i])
-            x22 = r[i] * np.sin(theta[i] + 0.5 * dtheta[i])
-            x21 = (r[i] + dr[i]) * np.sin(theta[i] + 0.5 * dtheta[i])
-            z11 = r[i] * np.cos(theta[i] - 0.5 * dtheta[i])
-            z12 = (r[i] + dr[i]) * np.cos(theta[i] - 0.5 * dtheta[i])
-            z22 = r[i] * np.cos(theta[i] + 0.5 * dtheta[i])
-            z21 = (r[i] + dr[i]) * np.cos(theta[i] + 0.5 * dtheta[i])
-        
+            if ((j == 1) | (j == 3)):
+                x11 = rL * np.sin(tL)
+                z11 = rL * np.cos(tL)
+                x12 = rH * np.sin(tL)
+                z12 = rH * np.cos(tL)
+                x21 = rH * np.sin(tH)
+                z21 = rH * np.cos(tH)
+                x22 = rL * np.sin(tH)
+                z22 = rL * np.cos(tH)
+            else:
+                x11 = -rL * np.sin(tL)
+                z11 = rL * np.cos(tL)
+                x12 = -rH * np.sin(tL)
+                z12 = rH * np.cos(tL)
+                x21 = -rH * np.sin(tH)
+                z21 = rH * np.cos(tH)
+                x22 = -rL * np.sin(tH)
+                z22 = rL * np.cos(tH)
+    
             xy = list(zip([x11, x12, x21, x22], [z11, z12, z21, z22]))
-        
+
             ax.add_patch(mpl.patches.Polygon \
                            (xy = xy, fill = False, alpha = MeshAlpha))
     
-    plt.gca().set_aspect('equal')
-    
-    # Colorbar code from
-    # https://stackoverflow.com/questions/32462881/add-colorbar-to-existing-axis
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes('right', size = '5%', pad = 0.05)
-    cb = fig.colorbar(mapper, cax = cax)
-    cb.set_label(r'{:}'.format(zLabel), fontsize = 16)
-    plt.savefig(figName, dpi = 300, bbox_inches = 'tight')
-    print('\n  Saved {:}'.format(figName))
-    #plt.show()
+ax.axhline(0.0, lw = 2, c = 'k')
+ax.axvline(0.0, lw = 2, c = 'k')
+plt.gca().set_aspect('equal')
+
+# Colorbar code from
+# https://stackoverflow.com/questions/32462881/add-colorbar-to-existing-axis
+divider = make_axes_locatable(ax)
+cax = divider.append_axes('top', size = '5%', pad = 0.05)
+cb = fig.colorbar(mapper, cax = cax, orientation = 'horizontal')
+cb.ax.xaxis.set_ticks_position('top')
+cb.ax.xaxis.set_label_position('top')
+cb.set_label(r'{:}'.format(zLabel), labelpad = 10.5, fontsize = 16)
+plt.savefig(figName, dpi = 300, bbox_inches = 'tight')
+print('\n  Saved {:}'.format(figName))
+#plt.show()
 
 import os
 os.system('rm -rf __pycache__')
